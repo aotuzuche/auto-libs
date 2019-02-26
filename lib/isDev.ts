2aaaabe3fbcc09b2353de83c88d3dbe4238fc72a
Eason chiu <76670758@qq.com>
2019-02-26 19:50:09 +0800

    fix(isDev.ts): 修改判定方式

--- lib/isDev.ts
+++ lib/isDev.ts
@@ -1,8 +1,18 @@
-let isDev = true;
 const env = (process as any).env;
 
-if ((env.PACKAGE && env.PACKAGE === 'production') || (env.REACT_APP_PACKAGE && env.REACT_APP_PACKAGE !== 'dev')) {
-  isDev = false;
+const isDev = () => {
+  // 旧版本脚手架
+  if (env.PACKAGE === 'production') {
+    return false;
+  }
+
+  // 新版本(旧版的PACKAGE变量所有环境都会存在，所以只要没有PACKAGE，就认为是新版本)
+  if (!env.PACKAGE && env.REACT_APP_PACKAGE !== 'dev') {
+    return false;
+  }
+
+  return true;
 }
 
-export default isDev;
+
+export default isDev();
