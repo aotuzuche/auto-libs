4b1b7903577d0faef820841bb7c9b1b583d50a5b
郭慧蓉 <huirong.guo@atzuche.com>
2019-07-24 11:17:21 +0800

    是否是小程序环境，完善

--- lib/miniprogram.ts
+++ lib/miniprogram.ts
@@ -1,16 +1,16 @@
 export const initMini = () => {
     const ua = navigator.userAgent;
-    const isWx = /MicroMessenger/gi.test(ua);
-    const isAlipay = /AlipayClient/gi.test(ua);
-    const isSwan = /swan/gi.test(ua);
+    const isWx = /MicroMessenger/gi.test(ua)
+    const isAlipay = /AlipayClient/gi.test(ua)
+    const isSwan = /swan/gi.test(ua)
     if (isAlipay) {
-        document.writeln('<script src="https://appx/web-view.min.js"' + '>' + '<' + '/' + 'script>');
+        document.writeln('<script src="https://appx/web-view.min.js"' + '>' + '<' + '/' + 'script>')
     }
     if (isSwan) {
-        document.writeln('<script src="https://b.bdstatic.com/searchbox/icms/searchbox/js/swan-2.0.6.js"' + '>' + '<' + '/' + 'script>');
+        document.writeln('<script src="https://b.bdstatic.com/searchbox/icms/searchbox/js/swan-2.0.6.js"' + '>' + '<' + '/' + 'script>')
     }
     if (isWx) {
-        document.writeln('<script src="https://res.wx.qq.com/open/js/jweixin-1.3.2.js"' + '>' + '<' + '/' + 'script>');
+        document.writeln('<script src="https://res.wx.qq.com/open/js/jweixin-1.3.2.js"' + '>' + '<' + '/' + 'script>')
     }
 }
 
@@ -23,48 +23,39 @@ interface MiniEnv {
 
 export const getMiniProgramEnv = (): Promise<MiniEnv> => {
     return new Promise(resolve => {
-        const data = {
-            isMiniProgram: true
-        };
         if (window.my && window.my.getEnv) {
             window.my.getEnv((res: any) => {
                 resolve(
-                    Object.assign(
-                        {
-                            isAlipay: res.miniprogram
-                        },
-                        data
-                    )
-                );
-            });
+                    {
+                        isAlipay: res.miniprogram,
+                        isMiniProgram: res.miniprogram
+                    }
+                )
+            })
         } else if (window.wx && window.wx.miniProgram) {
             window.wx.miniProgram.getEnv((res: any) => {
                 resolve(
-                    Object.assign(
-                        {
-                            isWeapp: res.miniprogram
-                        },
-                        data
-                    )
-                );
-            });
+                    {
+                        isWeapp: res.miniprogram,
+                        isMiniProgram: res.miniprogram
+                    }
+                )
+            })
         } else if (window.swan && window.swan.webView.getEnv) {
             window.swan.webView.getEnv((res: any) => {
                 resolve(
-                    Object.assign(
-                        {
-                            isSwan: res.smartprogram
-                        },
-                        data
-                    )
-                );
-            });
+                    {
+                        isSwan: res.smartprogram,
+                        isMiniProgram: res.smartprogram
+                    }
+                )
+            })
         } else {
             resolve(
-                Object.assign({
+                {
                     isMiniProgram: false
-                })
-            );
+                }
+            )
         }
-    });
-};
+    })
+}
