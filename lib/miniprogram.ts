fd6e861a62ff799364561dc7e7d8a1adebf02f62
郭慧蓉 <huirong.guo@atzuche.com>
2019-08-01 12:08:44 +0800

    miniprogram恢复到之前的0.1.46

--- lib/miniprogram.ts
+++ lib/miniprogram.ts
@@ -1,8 +1,8 @@
 export const initMini = () => {
-  const ua = navigator.userAgent;
-  const isWx = /MicroMessenger/gi.test(ua);
-  const isAlipay = /AlipayClient/gi.test(ua);
-  const isSwan = /swan/gi.test(ua);
+  const ua = navigator.userAgent
+  const isWx = /MicroMessenger/gi.test(ua)
+  const isAlipay = /AlipayClient/gi.test(ua)
+  const isSwan = /swan/gi.test(ua)
   if (isAlipay) {
     document.writeln('<script src="https://appx/web-view.min.js"' + '>' + '<' + '/' + 'script>');
   }
@@ -13,53 +13,49 @@ export const initMini = () => {
         '<' +
         '/' +
         'script>',
-    );
+    )
   }
   if (isWx) {
     document.writeln(
       '<script src="https://res.wx.qq.com/open/js/jweixin-1.3.2.js"' + '>' + '<' + '/' + 'script>',
-    );
+    )
   }
-};
+}
 
 interface MiniEnv {
-  isAlipay?: boolean;
-  isWeapp?: boolean;
-  isSwan?: boolean;
-  isMiniProgram: boolean;
+  isAlipay?: boolean
+  isWeapp?: boolean
+  isSwan?: boolean
+  isMiniProgram?: boolean
 }
 
 export const getMiniProgramEnv = (): Promise<MiniEnv> => {
   return new Promise(resolve => {
-    const defaultReturn: MiniEnv = {
-      isMiniProgram: true,
-    };
     if (window.my && window.my.getEnv) {
       window.my.getEnv((res: any) => {
         resolve({
-          ...defaultReturn,
           isAlipay: res.miniprogram,
-        });
-      });
+          isMiniProgram: res.miniprogram,
+        })
+      })
     } else if (window.wx && window.wx.miniProgram) {
       window.wx.miniProgram.getEnv((res: any) => {
         resolve({
-          ...defaultReturn,
           isWeapp: res.miniprogram,
-        });
-      });
-    } else if (window.swan && window.swan.webView && window.swan.webView.getEnv) {
+          isMiniProgram: res.miniprogram,
+        })
+      })
+    } else if (window.swan && window.swan.webView.getEnv) {
       window.swan.webView.getEnv((res: any) => {
         resolve({
-          ...defaultReturn,
           isSwan: res.smartprogram,
-        });
-      });
+          isMiniProgram: res.smartprogram,
+        })
+      })
     } else {
       resolve({
-        ...defaultReturn,
         isMiniProgram: false,
-      });
+      })
     }
-  });
-};
+  })
+}
