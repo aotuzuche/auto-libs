9bd1d5afa968ffd267edf9c8d9b8d53c64e2c2d1
mingzx <443456030@qq.com>
2019-11-05 18:06:43 +0800

    fix: 新增cdn地址

--- lib/cdn.ts
+++ lib/cdn.ts
@@ -1,7 +1,7 @@
 import isDev from './isDev';
 
 export const cdn = !isDev
-  ? '//carphoto.atzuche.com/'
-  : '//at-images-test.oss-cn-hangzhou.aliyuncs.com/';
+  ? 'https://carphoto.atzuche.com/'
+  : 'https://at-images-test.oss-cn-hangzhou.aliyuncs.com/';
 
 export const cdn_host = !isDev ? 'https://cdn.atzuche.com/' : 'https://cdn-test.atzuche.com/';
