c52ed2a03c02f52fb1a4c202cb040a6e9d6b691b
mingzx <443456030@qq.com>
2019-11-05 16:53:51 +0800

    修改cdn地址

--- lib/cdn.ts
+++ lib/cdn.ts
@@ -1,5 +1,6 @@
 import isDev from './isDev';
 
-export const cdn = !isDev
-  ? '//carphoto.atzuche.com/'
-  : '//at-images-test.oss-cn-hangzhou.aliyuncs.com/';
+export const cdn_host = !isDev
+  ? 'https://cdn.atzuche.com/'
+  : 'https://cdn-test.atzuche.com/';
+
