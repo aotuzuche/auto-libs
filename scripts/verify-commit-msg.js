ba32545bd024fdf828f11c9309d15408623a9b0f
shao <darryshaw@gmail.com>
2019-12-05 13:52:02 +0800

    workflow: change commit verify

--- scripts/verify-commit-msg.js
+++ scripts/verify-commit-msg.js
@@ -3,10 +3,11 @@ const msgPath = process.env.HUSKY_GIT_PARAMS;
 const msg = require('fs')
   .readFileSync(msgPath, 'utf-8')
   .trim();
+const packageJson = require('../package.json');
 
 const commitRE = /^(revert: )?(feat|fix|polish|docs|style|refactor|perf|test|workflow|ci|chore|types|build)(\(.+\))?: .{1,50}/;
 
-if (!commitRE.test(msg)) {
+if (!commitRE.test(msg) && packageJson.version !== msg) {
   console.log();
   console.error(`  ${chalk.bgRed.white(' ERROR ')} 无效的提交信息`);
   process.exit(1);
