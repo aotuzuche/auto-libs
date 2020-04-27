c4f047a96932a780da9302c161b5cd57738a5b13
Eason chiu <76670758@qq.com>
2020-04-27 10:32:52 +0800

    feat(Search): 将class转为对象，减少没必要的提示信息

--- lib/search.ts
+++ lib/search.ts
@@ -1,51 +1,51 @@
 import qs from 'qs';
 
-export default class Search {
+const Search = {
   // 获取search的map
-  public static parse<T extends any>(): T {
+  parse<T extends any>(): T {
     const sh = window.location.search.replace(/^\?/, '');
     const map = qs.parse(sh);
     Object.keys(map).forEach(k => {
       map[k] = decodeURIComponent(decodeURIComponent(map[k]));
     });
     return map;
-  }
+  },
 
   // 获取search的string
-  public static toString() {
+  toString() {
     return window.location.search.replace(/^\?/, '');
-  }
+  },
 
   // 获取search的指定key
-  public static get(key: string): string | undefined {
+  get(key: string): string | undefined {
     const val = Search.parse()[key];
     return val;
-  }
+  },
 
   // 获取search的指定key，不存在时使用默认值
-  public static getDefault(key: string, value: string): string {
+  getDefault(key: string, value: string): string {
     const res = Search.get(key);
     return typeof res === 'undefined' ? value : res;
-  }
+  },
 
   // 设置
-  public static set(key: string, value: string) {
+  set(key: string, value: string) {
     const map = Search.parse();
     map[key] = value;
     window.history.replaceState(null, '', `?${qs.stringify(map)}`);
     return Search;
-  }
+  },
 
   // 定义，即当不存在时才添加
-  public static define(key: string, value: string) {
+  define(key: string, value: string) {
     if (!Search.exist(key)) {
       Search.set(key, value);
     }
     return Search;
-  }
+  },
 
   // 删除
-  public static remove(key: string) {
+  remove(key: string) {
     const map = Search.parse();
     delete map[key];
     if (Object.keys(map).length === 0) {
@@ -54,26 +54,28 @@ export default class Search {
       window.history.replaceState(null, '', `?${qs.stringify(map)}`);
     }
     return Search;
-  }
+  },
 
   // 清空
-  public static clear() {
+  clear() {
     window.history.replaceState(null, '', window.location.origin + window.location.pathname);
     return Search;
-  }
+  },
 
   // 判断是否存在
-  public static exist(key: string) {
+  exist(key: string) {
     return typeof Search.get(key) !== 'undefined';
-  }
+  },
 
   // 判断是否为
-  public static is(key: string, value: string) {
+  is(key: string, value: string) {
     return Search.get(key) === value;
-  }
-}
+  },
+};
 
 export const search = () => {
   const search = window.location.search.replace(/^\?/, '');
   return qs.parse(search);
 };
+
+export default Search;
