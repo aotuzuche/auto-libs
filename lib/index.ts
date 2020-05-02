8eae3ad85eea03cefc67986b77c6a30a006618c5
Eason chiu <76670758@qq.com>
2020-05-03 03:12:05 +0800

    feat: 添加report方法

--- lib/index.ts
+++ lib/index.ts
@@ -8,6 +8,7 @@ export * from './transfer';
 export * from './setWechatTitle';
 export * from './miniprogram';
 export * from './search';
+export { default as Report } from './report';
 export { default as CDN } from './cdn';
 export { default as Reg } from './reg';
 export { default as Search } from './search';
