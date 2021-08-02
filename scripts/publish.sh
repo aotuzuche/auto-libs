660ae2edc42fe12df51c1ba614c65ee0573f4b0b
Eason chiu <76670758@qq.com>
2021-08-02 16:08:28 +0800

    ci: 修改发布功能

---
+++ scripts/publish.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo 'publish with proxy:'
+git config --global http.https://github.com.proxy http://127.0.0.1:10080
+git config --global https.https://github.com.proxy https://127.0.0.1:10080
+git config --global http.https://github.com.proxy
+git config --global https.https://github.com.proxy
+yarn np
+git config --global --unset http.https://github.com.proxy
+git config --global --unset https.https://github.com.proxy
